2c2b896822004721e937d84c8e52901de552961a
Bostjan Bozic <bozic.bostjann@gmail.com>
2019-04-30 12:46:39 +0200

    Added ELK, Fluentd and example

---
+++ infracoders-VI/logging-example/run.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+mkdir -p /data/infracoders/logs
+
+i=0
+
+while true; do
+    i=$((i+1))
+
+    LOG_DATE=`date "+%Y-%m-%dT%H:%M:%S"`
+    FILE_DATE=`date +%Y%m%d%H`
+
+    echo "${LOG_DATE};Welcome to InfraCoders!;Log loop ${i}" >> /data/infracoders/logs/test_log_${FILE_DATE}.log
+    sleep 5
+done
