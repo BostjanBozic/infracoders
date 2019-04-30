e6ea9671397f58a65c9c7a58f087fdefab5b57b0
Bostjan Bozic <bozic.bostjann@gmail.com>
2019-04-30 17:23:46 +0200

    Updated Fluentd gem version to v1.4.2

--- infracoders-VI/logging-example/run.sh
+++ infracoders-VI/logging-example/run.sh
@@ -10,6 +10,6 @@ while true; do
     LOG_DATE=`date "+%Y-%m-%dT%H:%M:%S"`
     FILE_DATE=`date +%Y%m%d%H`
 
-    echo "${LOG_DATE};Welcome to InfraCoders!;Log loop ${i}" >> /data/infracoders/logs/infracoders_log_${FILE_DATE}.log
+    echo "${LOG_DATE};Welcome to InfraCoders!;${i}" >> /data/infracoders/logs/infracoders_log_${FILE_DATE}.log
     sleep 5
 done
